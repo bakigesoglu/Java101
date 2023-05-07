350b31837c858705233aeb11efac4946e2dc88b0
Baki Gesoğlu <bakigesoglu1@gmail.com>
2023-05-07 22:03:19 +0300

    FourfoldTotal

---
+++ OddNumbers.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+/*
+@author alfonso
+ */
+public class OddNumbers {
+    public static void main(String[] Args) {
+        Scanner inp = new Scanner(System.in);
+        int number, sum = 0;
+
+
+        do {
+            System.out.print("Enter a number: ");
+            number = inp.nextInt();
+            if  (number % 4 == 0) {
+                sum += number;
+            }
+            else if (number%2 != 0)
+            {
+                System.out.println("You entered odd number.");
+            }
+
+        } while (number % 2 == 0);
+
+         System.out.print("Total: "+ sum);
+
+    }
+}
+
