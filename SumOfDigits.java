f962c1c4b0a24780e796627a235dce5c95a2a49c
Baki Gesoğlu <bakigesoglu1@gmail.com>
2023-05-13 01:25:09 +0300

    SumOfDigits

---
+++ SumOfDigits.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+/*
+@author alfonso
+ */
+public class SumOfDigits {
+    public static void main (String [] Args) {
+
+        Scanner inp = new Scanner(System.in);
+        int number,remain,total=0;
+        System.out.print("Enter a number: ");
+        number = inp.nextInt();
+
+        while (number >= 1)
+        {
+            remain = number % 10 ;
+            total+=remain;
+            number/=10;
+        }
+          System.out.print(total);
+    }
+}
