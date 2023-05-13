9ce715222efb565f381330da700eae24d553115e
Baki Gesoğlu <bakigesoglu1@gmail.com>
2023-05-13 16:41:30 +0300

    Diamond

---
+++ Diamond.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+/*
+@author alfonso
+ */
+public class Diamond {
+    public static void main (String [] Args) {
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Enter number of steps: ");
+        int n= inp.nextInt();
+
+        for (int i=1; i <= n ; i++){
+            for (int k = 0; k < (n-i); k++ )
+            {
+                System.out.print(" ");
+            }
+
+            for (int j=1; j<= (2*i-1); j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int a= n-1; a >= 0; a--)
+        {
+            for (int b=0; b < n-a; b++)
+            {
+                System.out.print(" ");
+            }
+
+            for (int c=1; c<=(2*a-1); c++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
